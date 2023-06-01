40f6399f9c8114189777de3a9ec6269dc00e4af7
Alexandro G Dos Santos <alexandrogonsan@outlook.com>
2023-05-31 21:13:01 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+apt update
+pkg update
+apt upgrade
+pkg upgrade
+
+pkg install clang python fftw libjpeg-turbo libpng pkg-config
+
+python -m venv venv
+pip install -r requirements.txt
+
+mkdir lib64
+cd lib64
+
+git clone https://github.com/opencv/opencv.git
+cd opencv
+
+mkdir build
+cd build
+
+cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr/local -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
+make -j8
+make install
